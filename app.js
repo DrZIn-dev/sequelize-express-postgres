12bd2ffbe2f645cd845095ebd79a01c93bcb9329
DrZin-dev <52484219+DrZIn-dev@users.noreply.github.com>
2020-05-28 16:56:42 +0700

    init project

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`)
+})
