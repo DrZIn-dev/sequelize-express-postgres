231155010c1d970b6a4562c418800f19b10db5bc
DrZin-dev <52484219+DrZIn-dev@users.noreply.github.com>
2020-05-28 16:58:12 +0700

    update config

---
+++ database/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    database: process.env.DEV_DB_NAME,
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    host: process.env.DEV_DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    database: process.env.TEST_DB_NAME,
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASS,
+    host: process.env.TEST_DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    database: process.env.PRODUCTION_DB_NAME,
+    username: process.env.PRODUCTION_DB_USER,
+    password: process.env.PRODUCTION_DB_PASS,
+    host: process.env.PRODUCTION_DB_HOST,
+    dialect: 'postgres',
+  },
+}
